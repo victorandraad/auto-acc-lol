a5a5fbf165985ccab20813c7ada707800070a11c
Mitters <vitinhodaniel887@gmail.com>
2023-03-11 22:41:08 -0300

    botão autopick e accept adicionado

--- Interface/main.py
+++ Interface/main.py
@@ -1,22 +1,45 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
 import sys
 
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        
+        self.style_autoaccept = False
+        self.style_autopick = False
 
-        self.setWindowTitle('p e p p a')
+        self.autoaccept = QPushButton('auto-accept', self)
+        self.autoaccept.setObjectName('auto')
+        self.autoaccept.setGeometry(10, 10, 130, 30)
+        self.autoaccept.clicked.connect(self.autoaccept_action)
+
+        self.autopick = QPushButton('auto-pick', self)
+        self.autopick.setObjectName('auto')
+        self.autoaccept.setGeometry(200, 10, 130, 30)
+        self.autopick.clicked.connect(self.autopick_action) 
+
+        self.setWindowTitle('p e p p a . j p e g')
         self.setGeometry(100, 100, 400, 300)
-        self.setStyleSheet('background-color: black;')
+        self.setObjectName('body')
+        with open('Interface\style.css', 'r') as f:
+            css = f.read()     
+        app.setStyleSheet(css)
 
-        button = QPushButton('Botão', self)
-        button.setGeometry(150, 100, 100, 50)
+    def autoaccept_action(self):
+        self.style_autoaccept = not self.style_autoaccept
 
-        webview = QWebEngineView(self)
-        self.setCentralWidget(webview)
-        webview.load(QUrl(''))
+        if self.style_autoaccept:
+            self.autoaccept.setStyleSheet('background-color: #537FE7;')
+        else:
+            self.autoaccept.setStyleSheet('background-color: black;')
+
+    def autopick_action(self):
+        self.style_autopick = not self.style_autopick
+
+        if self.style_autopick:
+            self.autopick.setStyleSheet('background-color: #537FE7;')
+        else:
+            self.autopick.setStyleSheet('background-color: black;')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
