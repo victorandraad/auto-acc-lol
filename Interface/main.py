b57148b857e8c2c49e9ab66e6b209c757b895877
Mitters <vitinhodaniel887@gmail.com>
2023-03-12 21:56:30 -0300

    ultimo commit antes de deletar class change

--- Interface/main.py
+++ Interface/main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFrame, QWidget, QVBoxLayout, QScrollArea
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 import sys
 import os
 
@@ -108,6 +108,7 @@ class Change(QWidget):
         self.setWindowTitle('p e p p a . j p e g')
         self.setGeometry(20, 100, 360, 350)
         self.setObjectName('body')
+        self.setFixedSize(350, 350)
 
         with open('Interface/style.css', 'r') as f:
             css = f.read()     
@@ -117,7 +118,7 @@ class Change(QWidget):
         self.image_dir = 'Interface\settings\champs'
         self.images_files = [f for f in os.listdir(self.image_dir) if os.path.isfile(os.path.join(self.image_dir,  f)) and f.endswith('.png')]
         
-
+        # Cria um widget
         # Lista todos os campeões
         contador = -5
         contador2 = 0
@@ -144,7 +145,7 @@ class Change(QWidget):
                     label.setPixmap(image)
                     layout.addWidget(label)
                     layout.setContentsMargins(10, 0, 0, 0)
-                    button.setGeometry(c*70, contador2*75, image.width(), image.height())
+                    button.setGeometry(c*70, contador2*70, image.width(), image.height())
                     contador +=1
             contador2+=1 
 
--- Interface/teste.py
+++ Interface/teste.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QFrame, QWidget, QVBoxLayout, QScrollArea
 from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QIcon
 import sys
 import os
 
@@ -11,19 +11,15 @@ class Change(QWidget):
         self.setWindowTitle('p e p p a . j p e g')
         self.setGeometry(20, 100, 360, 350)
         self.setObjectName('body')
+        self.setFixedSize(350, 350)
 
         with open('Interface/style.css', 'r') as f:
             css = f.read()     
         app.setStyleSheet(css)
 
-        #Creating a widget with all content
-        self.widget = QWidget(self)
-        self.scroll_Qvbox = QVBoxLayout(self.widget)
-
         # Botões imagem
         self.image_dir = 'Interface\settings\champs'
         self.images_files = [f for f in os.listdir(self.image_dir) if os.path.isfile(os.path.join(self.image_dir,  f)) and f.endswith('.png')]
-        
 
         # Lista todos os campeões
         contador = -5
@@ -32,39 +28,28 @@ class Change(QWidget):
         for c in range(0, atingir, 5):
             for c in range(0, 5):
                 if contador < 0:
-                    button = QPushButton(self.widget)
+                    button = QPushButton(self)
                     layout = QVBoxLayout(button)
                     image = QPixmap(os.path.join(self.image_dir, self.images_files[c])).scaled(60, 60)
-                    label = QLabel(self.widget)
+                    label = QLabel(self)
                     label.setPixmap(image)
-                    self.scroll_Qvbox.addWidget(label)
-                    self.scroll_Qvbox.setContentsMargins(10, 0, 0, 0)
+                    layout.addWidget(label)
+                    layout.setContentsMargins(10, 0, 0, 0)
                     button.setGeometry(c*70, 10, image.width(), image.height())
                     contador +=1
                 elif contador >= atingir-5:
                     print('oi')
                 elif contador >=0:
-                    button = QPushButton(self.widget)
+                    button = QPushButton(self)
                     layout = QVBoxLayout(button)
                     image = QPixmap(os.path.join(self.image_dir, self.images_files[contador+5])).scaled(60, 60)
-                    label = QLabel(self.widget)
+                    label = QLabel(self)
                     label.setPixmap(image)
-                    self.scroll_Qvbox.addWidget(label)
-                    self.scroll_Qvbox.setContentsMargins(10, 0, 0, 0)
-                    button.setGeometry(c*70, contador2*75, image.width(), image.height())
+                    layout.addWidget(label)
+                    layout.setContentsMargins(10, 0, 0, 0)
+                    button.setGeometry(c*70, contador2*70, image.width(), image.height())
                     contador +=1
-            contador2+=1 
-    
-        
-        # Create thhe scroll area and set its widget property
-        self.scroll_area = QScrollArea()
-        self.scroll_area.setWidget(self.widget)
-
-        # Add the scroll  area to the  parent widget's layout
-        main_widget = QWidget()
-        main_layout = QVBoxLayout(main_widget)
-        main_layout.addWidget(self.scroll_area)
-
+                    
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = Change()
