26ca29e62f12cbdaadde5769dfb4c989288e9458
vouto <68758638+andradavic@users.noreply.github.com>
2023-09-12 23:21:49 -0300

    first layout

--- functions.py
+++ functions.py
@@ -1,90 +1,55 @@
-#Release 2.0
-from pytesseract import pytesseract, image_to_string
-from pyautogui import FAILSAFE, PAUSE, screenshot, position, click, moveTo, write
-from pygetwindow import getAllTitles, getWindowsWithTitle
-from time import sleep
-from threading import Thread
-from tkinter import *
-from interface import config, language
+import json
+from lcu_driver import Connector
+connector = Connector()
 
-PAUSE = config['delay']
-PAUSE = float(PAUSE)
-FAILSAFE = False
+async def champselect(connection):
+    champSelect = await connection.request('get', 'lol/-champ-select/v1/session')
+    if champSelect.status != 200:
+        pass
+    else: test = json.loads(await champSelect.read())
+    print(test)
 
-class Start:
-  
-    def get_text(self, value, search=(int, int, int, int)):
-        listWindow = getAllTitles()  #lista nome de todos os apps ativos no windows
-        if 'League of Legends (TM) Client' in listWindow:
-            sleep(60)
-            return
-        
-        else:
-            try:
-                hwnd_lol = getWindowsWithTitle('League of Legends')[0] # pega o codigo da janela do lol
-                self.left, self.top, self.width, self.height = hwnd_lol.left, hwnd_lol.top, hwnd_lol.width, hwnd_lol.height
-                
-                x1, y1, x2, y2 = (search[0] / 1024) * self.width, (search[1] / 576) * self.height, (search[2] / 1024) * self.width, (search[3] / 576) * self.height
-                self.region = (self.left + x1, self.top +y1, x2 - x1, y2 - y1)
 
-                appscreenshot = screenshot(region=(self.region))
-                pytesseract.tesseract_cmd = r'Tesseract-OCR\tesseract.exe'
-            except IndexError:
-                pass
+@connector.ready
+async def connect(connection):
+    print("It has connected to league")
 
-            try:
-                texto = image_to_string(appscreenshot)
-                if value in texto:
-                    return True
-                sleep(4)
-            except (AttributeError, SystemError, UnboundLocalError):
-                pass
+@connector.close
+async def disconnect(connection):
+    print("It has disconnected from league")
 
-    def click_point(self, region=(int, int), rverify=(int, int, int, int)):
-        rx, ry = position()
-        s1 = screenshot(region=(rverify[0], rverify[1], rverify[2], rverify[3]))
-        sleep(PAUSE)
-        click(self.left + (region[0] / 1024) * self.width, self.top + (region[1] / 576) * self.height, duration=0)
-        moveTo(rx, ry, duration=0)
+@connector.ws.register("/lol-champ-select/v1/session", event_types=("UPDATE",))
+async def updated(connection, event):
+    await champSelect(connection)
 
-        if s1 == screenshot(region=(rverify[0], rverify[1], rverify[2], rverify[3])):
-            return False
 
-    def autoacc(self, Nada=None):
-        while self.acc_on:
-            if self.get_text(self=Start, search=(475, 436, 551, 453), value=language['accepttext']):
-                if self.click_point(self=Start, region=(475, 436), rverify=(475, 436, 551, 453)) == False:
-                    pass
-                else:
-                    self.sleepAcc = sleep(12)
 
-    def autopick(self, Nada=None):
-        while self.autopick_on:
-            if self.get_text(self=Start, search=(475, 478, 551, 493), value=language['confirmbutton_text']):
-                self.click_point(self=Start, region=(631, 84), rverify=(588, 73, 615, 92)) #clicar no buscar
-                write(self.pick)
+@connector.ws.register("/lol-pre-end-of-game/v1/currentSequenceEvent", event_types=("UPDATE",))
+async def updated(connection, event):
+    print()
+    end = await connection.request("get", "/lol-pre-end-of-game/v1/currentSequenceEvent")
+    end = json.loads(await end.read())
+    print(end)
 
-                self.click_point(self=Start, region=(309, 128), rverify=(283, 106, 335, 158)) #clicar no champ
+@connector.ws.register("/lol-matchmaking/search", event_types=("UPDATE",))
+async def updated(connection, event):
+    print()
+    end = await connection.request("get", "/lol-matchmaking/search")
+    end = json.loads(await end.read())
+    print(end)
 
-                self.click_point(self=Start, region=(506, 488), rverify=(465, 369, 569, 410)) #clicar em confirmar
+@connector.ws.register("/lol-matchmaking/v1/ready-check", event_types=("UPDATE",))
+@connector.ws.register("/lol-matchmaking/v1/ready-check/decline", event_types=("CREATE",))
+async def decline(connection, event):
+    print()
+    end = await connection.request("get", "/lol-matchmaking/v1/ready-check")
+    end = json.loads(await end.read())
+    print(end)
+    if end['state'] == "InProgress":
+        end = await connection.request("post", "/lol-matchmaking/v1/ready-check/accept")
+        end = json.loads(await end.read())
+        print(end)
 
-                self.sleepPick = sleep(12)
 
-    def onoff_threads(self, method=None, champ=None):
-        self.pickThread = Thread(target=self.autopick, args=(Start, None))
-        self.accThread = Thread(target=self.autoacc, args=(Start, None))
-        self.pick = champ
 
-        if method == 'pick__off':
-            self.autopick_on = False
-
-        elif method == 'pick__on':
-            self.autopick_on = True
-            self.pickThread.start()
-
-        if method == 'acc__off':
-            self.acc_on = False
-
-        elif method == 'acc__on':
-            self.acc_on = True
-            self.accThread.start()
+connector.start()
--- interface.py
+++ interface.py
@@ -1,167 +1,49 @@
-#Release  2.0
-from tkinter import *
-from data import *
-from functions import *
-from time import sleep
-from threading import Thread
-import urllib.request
-import os
-import yaml
-import locale
+import time
 
-with open('config.yaml') as f:
-    config = yaml.safe_load(f)
+import flet as ft
+from datetime import datetime
 
-if config['language'] == '':
-    win_language = locale.getdefaultlocale()
-    config['language'] = win_language[0] + win_language
-        
-with open('config.yaml', 'w') as f:
-    yaml.dump(config, f)
 
-language_Inicials = config['language'][0], config['language'][1]
+def main(page: ft.Page):
 
-with open(f'languages\{language_Inicials[0]}{language_Inicials[1]}.yaml', 'r') as l:
-    language = yaml.safe_load(l)
+    page.title = "LCULeagueTools"
+    page.fonts = {
+        "Jomhuria": "https://fonts.googleapis.com/css?family=Jomhuria"
+    }
+    page.window_width = 468
+    page.window_height = 403
+    page.bgcolor = "#1E272E"
 
-class App:
-    def __init__(self):
-        self.window = Tk()
-        self.window.geometry("359x265")
-        self.window.title("Woontt-")
-        self.window.config(bg='#202020')
-        self.window.iconbitmap('Woontt.ico')
-        self.window.resizable(width=False, height=False)
+    acceptbtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoAccept")
+    autorunebtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoRuna")
+    autospellsbtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoSpells")
+    autobanbtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoBan")
+    autododgebtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoDodge")
+    automatchmakingbtn = ft.Checkbox(check_color="#0BE881", fill_color="#485460", width=124, height=39, label="AutoMatchMaking")
+    page.add(
+        ft.Row(
+            controls=[
+                ft.Column(
+                    [
+                        acceptbtn,
+                        autorunebtn,
+                        autospellsbtn,
+                    ]
+                ),
 
-        self.search_assistant = ''
-        self.champ = config['last_champion']
-        self.index = config['index_champion']
-        self.pick_status = BooleanVar()
-        self.acc_status = BooleanVar()
+                ft.Column(
+                    [
+                        autobanbtn,
+                        autododgebtn,
+                        automatchmakingbtn,
+                    ]
+                )
+            ]
+        )
+    )
 
-        self.autopick_button = Checkbutton(self.window, width=9, height=1, text="Autopick", bg='#C33128', fg='black', indicatoron=0, activebackground='#C33128', activeforeground='black', font=('Imperial 12 bold'), command=self.autopick_, variable=self.pick_status, onvalue=True, offvalue=False)
-        self.autopick_button.place(x=219, y=50)
+    pass
 
-        self.autoacc_button = Checkbutton(self.window, width=9, height=1, text="Autoacc", bg='#C33128', fg='black', indicatoron=0, activebackground='#C33128', activeforeground='black', font=('Imperial 12 bold'), command=self.autoacc_, variable=self.acc_status, onvalue=True, offvalue=False)
-        self.autoacc_button.place(x=219, y=100)
 
-        # self.language_list = Listbox(self.window, width=10, height=1, bg='#202020', fg="white", font=("Imperial 12 bold"),)
-        # self.language_list.place(x=221, y=130)
-
-        self.champImage = Label(self.window)
-        self.champImage.place(x=35,y=40)
-
-        #put last champ image
-        self.champ_image = PhotoImage(file=rf'champs\{self.champ}.png')
-        self.champImage['image'] = self.champ_image
-
-        self.champList = Listbox(self.window, width=12, height=1, bg='#C33128', fg="black", font=("Imperial 12 bold"),)
-        self.champList.place(x=41, y=168)
-
-        #insert all champions name
-        champs, version = get_champs()
-        del(version)
-        for n, champ in enumerate(champs):
-            self.champList.insert(n, champ)
-
-        #insert all default languages
-        # langs = os.listdir('languages')
-        # for n, lang in enumerate(langs):
-        #     if lang == 'en.yaml':
-        #          lang = 'English'
-
-        #     if lang == 'es.yaml':
-        #          lang = 'Español'
-
-        #     if lang == 'pt.yaml':
-        #          lang = 'Português'
-        #     self.language_list.insert(n, lang)
-        
-        self.champList.see(config['index_champion']) # See last champ name
-        self.champList.bind('<KeyRelease>', self.on_keyrelease)
-
-        confirm = Button(self.window,  width=10, height=1, font=("Imperial 12 bold"), text=language['confirmbutton_text'], command=self.confirm__)
-        confirm.place(x=138, y=220)
-    
-    def show_image(self):
-        try:
-            self.index = self.champList.curselection()[0]
-        except IndexError:
-            self.index = 0
-
-        self.champ = self.champList.get(self.index)
-        files = os.listdir('champs')
-        
-        # print(self.champList.get(self.index)) Nome do campeão selecionado
-        if not f'{self.champ}.png' in files:
-            url = get_image()[self.index]
-            filename = rf'champs\{self.champ}.png'
-            urllib.request.urlretrieve(url, filename)
-
-        self.champ_image = PhotoImage(file=rf'champs\{self.champ}.png')
-        self.champImage['image'] = self.champ_image
-    
-    def on_keyrelease(self, event):
-        letter = event.char
-        letter = letter.lower()
-        key = event.keysym
-        if letter:
-            self.search_assistant += letter
-            self.search_assistant = self.search_assistant.capitalize()
-        elif key == 'BackSpace':
-            self.search_assistant = ''
-            self.search_assistant = self.search_assistant.capitalize()
-
-        list, v = get_champs()
-        del(v)
-        for self.champ in list:
-            if self.search_assistant in self.champ:
-                j = self.champList.get(0, END).index(self.champ)
-                print(j)
-                self.champList.select_clear(0, END)
-                self.champList.selection_set(j)
-                self.champList.activate(j)
-                self.champList.see(j)
-                self.champ = list[j]
-                break
-
-    def autopick_(self):
-        if self.pick_status.get():
-            Start.onoff_threads(Start, 'pick__on', self.champ)
-
-        else:
-            Start.onoff_threads(Start, 'pick__off')
-            cdThread = Thread(target=self.cd, args=(self.autopick_button, 12, 'Autopick', self.autopick_))
-            cdThread.start()
-
-    def autoacc_(self):
-        if self.acc_status.get():
-            Start.onoff_threads(Start, 'acc__on')
-        else:
-            Start.onoff_threads(Start, 'acc__off')
-            cdThread = Thread(target=self.cd, args=(self.autoacc_button, 12, 'Autoacc', self.autoacc_))
-            cdThread.start()
-  
-    def confirm__(self):
-        self.show_image()
-        self.search_assistant = ''
-
-        config['last_champion'] = self.champ
-        config['index_champion'] = self.index
-        with open('config.yaml', 'w') as f:
-            yaml.dump(config, f)
-        Start.onoff_threads(Start, None, config['last_champion'])
-
-    def cd(self, button, time, default_message, default_command):
-        for c in range(time, 0, -1):
-            button['text'] = f'in {c} sec'
-            button['command'] = ''
-            sleep(1)
-        button['text'] = default_message
-        button['command'] = default_command
-
-if __name__ == '__main__':
-    app = App()
-    app.window.mainloop()
-    Start.onoff_threads(Start, 'pick__off')
-    Start.onoff_threads(Start, 'acc__off')
+if __name__ =="__main__":
+    ft.app(target=main, assets_dir="assets", view=ft.WEB_BROWSER)
